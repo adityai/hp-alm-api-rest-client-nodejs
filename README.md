d07d4578f367335727b466d76d18a0a3dd94c449
Alex Silva Gomes <alegomes@inmetrics.com.br>
2020-10-22 11:23:09 -0300

    commit inicial;

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# hp-alm-api-rest-client-nodejs
+- Enter HP ALM REST API URL: http://youralmserver:port/qcbin/api/authentication/sign-in
+- Enter your ALM User Name and Password in the prompt that you get on navigating to the above URL.
+- Once successful authentication has occurred you will get a blank page.
+- Now enter the URL to get domains in the same browser window: http://youralmserver:port/qcbin/api/domains
+- Once you enter this URL and press enter you will get domain list you have access to in XML format.
+
+WARN: For ALM Versions prior to 12.53, in URLs, replace api with rest
+e.g. http://youralmserver:port/qcbin/rest/domains
+
+
+referencias:
+> https://aneejian.com/hp-alm-rest-api-alm-domains/
+> https://aneejian.com/hp-alm-rest-api-alm-domains-projects/
+> https://community.microfocus.com/t5/ALM-QC-User-Discussions/Cannot-access-any-entity-in-ALM-12-2-via-REST/m-p/944762
+
+requirements:
+> npm install xml2json
---
+++ src/get-alm-data.js
@@ -0,0 +1,124 @@
+
+const XmlParser = require("xml2json");
+var http = require('http'),
+    fs = require('fs'),
+    config = JSON.parse(fs.readFileSync('config.json'));//this refers to a file where I have all my config like host, userName, password Etc
+
+// set the correct options for the call.
+var options = {
+    host : config.host, 
+    path : "/qcbin/api/authentication/sign-in",
+    method: "GET",
+    headers : {'Authorization': 'Basic '+ Buffer.from((config.alm_userName + ':' + config.alm_password)).toString('base64')}
+};
+
+// authenticating the user into ALM
+ALMConnect(options, 'header','', function(status, data){
+    if (status){
+        cookies = data.headers["set-cookie"]
+        //get the LWSSO_Cookie from the header. This is the session cookie which will be used in all callouts to ALM.
+        if (cookies != undefined ) {
+            
+            // is-authenticated
+            // 1. Lista de todos os domínios e projeto:
+            //var jsonDomains = Get(cookies, "/qcbin/api/domains")
+            Get(cookies, "/qcbin/api/domains?include-projects-info=y")
+            //Get(cookies, "/qcbin/api/domains")
+
+            // 2. Pesquisa de versão atrelada a uma sequência especifica:
+            //Get(cookies, "/qcbin/rest/domains/ARQUITETURA_TESTE_INTEGRACAO/projects/CTAS_CONTAS/releases?query={user-template-01[2020-0589902-5-002]}")
+
+            // 3. Lista dos ciclos:
+            //Get(cookies, "/qcbin/rest/domains/ARQUITETURA_TESTE_INTEGRACAO/projects/CTAS_CONTAS/release-cycles")
+
+            // 4. Lista de execuções de testes:
+            //Get(cookies, "/qcbin/rest/domains/ARQUITETURA_TESTE_INTEGRACAO/projects/CTAS_CONTAS/test-instances")
+                        
+            // 5. Lista de Defeitos:
+            //Get(cookies, "/qcbin/rest/domains/ARQUITETURA_TESTE_INTEGRACAO/projects/CTAS_CONTAS/defects")
+        } else{
+            console.log('ERROR:  Unable to login, check your username/password/serverURL.');
+        }
+    } else {
+        console.log('ERROR:  ' + JSON.stringify(data));
+    }
+});
+
+
+        
+
+function Get(LWSSO_Cookie, path){
+    
+    var XMLoutput='';
+    var JSONoutput='';
+    
+
+    //set the correct options for the call.
+    var options = {
+        host : config.host, 
+        path : path, 
+        method: "GET",
+        headers : {"Cookie":LWSSO_Cookie}
+    };
+
+    var request = http.request(options, function(res){
+        res.setEncoding('utf8');
+        
+        var Body = "";
+
+        res.on('data',function(chunk){
+            XMLoutput+=chunk;
+        });        
+
+        res.on('end',function(){
+            //console.log('XMLoutput: ' + XMLoutput);
+
+            JSONoutput = XmlParser.toJson(XMLoutput);
+            JSONoutput = JSON.parse(JSONoutput)
+
+            console.log("");
+            console.log('URI: '+ path);
+            
+            // console.log('XMLoutput:'); 
+            // console.log(XMLoutput);  
+            console.log('JSONoutput:');   
+            console.log("");
+            console.log(JSON.stringify(JSONoutput));     
+             
+        });
+
+    });
+
+    request.on('error',function(e){
+        callback(false,e);
+    });
+    if(options.method=='POST' || options.method == 'PUT'){
+        request.write(requestBody);
+    }
+    request.end();
+}
+
+function ALMConnect(opt, responseType, requestBody, callback){
+ 
+    var request = http.request(opt, function(res){
+        res.setEncoding('utf8');
+        var XMLoutput='';
+        res.on('data',function(chunk){
+            XMLoutput+=chunk;
+        });
+        res.on('end',function(){
+            if(responseType=='data'){
+                callback(true,XMLoutput);
+            }else {
+                callback(true, res);
+            }
+        });
+    });
+    request.on('error',function(e){
+        callback(false,e);
+    });
+    if(opt.method=='POST' || opt.method == 'PUT'){
+        request.write(requestBody);
+    }
+    request.end();
+}
